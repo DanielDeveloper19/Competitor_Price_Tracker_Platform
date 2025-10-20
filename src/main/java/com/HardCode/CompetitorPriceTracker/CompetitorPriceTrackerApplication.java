722f6197bd6c36cdcb3a087625f79c46958d636f
DanielDeveloper19 <daniel256531@gmail.com>
2025-10-20 11:23:28 -0500

    first commit, the begining

---
+++ src/main/java/com/HardCode/CompetitorPriceTracker/CompetitorPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.HardCode.CompetitorPriceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitorPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitorPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HardCode/CompetitorPriceTracker/CompetitorPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HardCode.CompetitorPriceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitorPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
